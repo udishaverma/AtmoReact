def469a4df42459cedc3ba3dfd843a7532f5beeb
udishaverma <udishaverma13@gmail.com>
2025-08-03 00:43:21 +0530

    first commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,13 +1,18 @@
 import React, { useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
+import humidity_icon from '../assets/humidity.png'
+import wind_icon from '../assets/wind.png'
 import clear_icon from '../assets/clear.png'
 import cloud_icon from '../assets/cloud.png'
 import drizzle_icon from '../assets/drizzle.png'
-import humidity_icon from '../assets/humidity.png'
 import rain_icon from '../assets/rain.png'
 import snow_icon from '../assets/snow.png'
-import wind_icon from '../assets/wind.png'
+import thunderstorm_icon from '../assets/thunderstorm.png'
+import night_clear_icon from '../assets/nightclear.png'
+import night_cloud_icon from '../assets/nightcloud.png'
+import night_rain_icon from '../assets/drizzle.png'
+import mist_icon from '../assets/mist.png'
 
 const Weather = () => {
 
@@ -16,19 +21,23 @@ const Weather = () => {
 
     const allIcons = {
         "01d": clear_icon,
-        "01n": clear_icon,
+        "01n": night_clear_icon,
         "02d": cloud_icon,
-        "02n": cloud_icon,
+        "02n": night_cloud_icon,
         "03d": cloud_icon,
-        "03n": cloud_icon,
+        "03n": night_cloud_icon,
         "04d": drizzle_icon,
         "04n": drizzle_icon,
         "09d": rain_icon,
-        "09n": rain_icon,
+        "09n": night_rain_icon,
         "10d": rain_icon,
-        "10n": rain_icon,
+        "10n": night_rain_icon,
+        "11d": thunderstorm_icon,
+        "11n": thunderstorm_icon,
         "13d": snow_icon,
-        "13n": snow_icon
+        "13n": snow_icon,
+        "50d": mist_icon,
+        "50n": mist_icon
     };
 
 
